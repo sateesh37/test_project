2e1ed422c1f2278a15850f95951f27c6533d3f50
pradeep <pradeepkumar@railsfactory-Veriton-Series>
2011-12-29 20:00:12 +0530

    adding Changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '2cb65de9cccd546270c81da70fad35d1142db9e4c5aa8c82163e1766c43c531972924a71d360435397e0d1ee12e574ae91ce4c219daaef98bd388d75ff9e6016'
